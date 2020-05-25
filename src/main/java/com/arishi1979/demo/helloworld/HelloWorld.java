91c7619681963791fd0c7529e2ce21240e1107b3
arishi1979 <arishi1979@hotmail.com>
2020-05-25 10:25:18 +0000

    Minor change to output

--- src/main/java/com/arishi1979/demo/helloworld/HelloWorld.java
+++ src/main/java/com/arishi1979/demo/helloworld/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.arishi1979.demo.helloworld;
+package src.main.java.com.arishi1979.demo.helloworld;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -9,7 +9,7 @@ public class HelloWorld {
     final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
     final LocalDateTime now = LocalDateTime.now();
 
-    System.out.println("Hello, World! The current time is " + dtf.format(now));
+    System.out.println("Hello World!! The current time is " + dtf.format(now));
   }
 
 }
